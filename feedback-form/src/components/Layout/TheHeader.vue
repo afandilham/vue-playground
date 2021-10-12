570b5640edb6785519904fdd2c74c50dad13aa10
Moch. Ilham Afandi <afandilham@gmail.com>
2021-10-12 15:26:42 +0700

    Delete feedback-form directory

--- feedback-form/src/components/Layout/TheHeader.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <base-card class="flex flex-col items-center justify-center">
-    <svg class="relative" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#6366f1" fill-opacity="1" d="M0,192L30,176C60,160,120,128,180,96C240,64,300,32,360,53.3C420,75,480,149,540,160C600,171,660,117,720,90.7C780,64,840,64,900,80C960,96,1020,128,1080,160C1140,192,1200,224,1260,218.7C1320,213,1380,171,1410,149.3L1440,128L1440,0L1410,0C1380,0,1320,0,1260,0C1200,0,1140,0,1080,0C1020,0,960,0,900,0C840,0,780,0,720,0C660,0,600,0,540,0C480,0,420,0,360,0C300,0,240,0,180,0C120,0,60,0,30,0L0,0Z"></path></svg>
-    <div class="header-content absolute mx-auto flex flex-col justify-center items-center">
-      <h1 class="text-center text-2xl md:text-5xl text-gray-700 font-extrabold my-4">Feedback Form</h1>
-    </div>
-  </base-card>
-</template>
--- feedback-form/src/components/UI/BaseButton.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <button class="p-4 bg-gradient-to-r from-blue-400 to-blue-600 text-gray-200 text-xs md:text-base sm:text-sm font-semibold capitalize">
-    <slot></slot>
-  </button>
-</template>
