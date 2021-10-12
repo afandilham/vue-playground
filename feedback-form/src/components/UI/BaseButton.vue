440bf5566026f0d8084b0d1ab49e0cee0115663a
unknown <afandilham@gmail.com>
2021-10-12 17:02:43 +0700

    Add feedback-form project

---
+++ feedback-form/src/components/UI/BaseButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <button class="p-4 bg-gradient-to-r from-blue-400 to-blue-600 text-gray-200 text-xs md:text-base sm:text-sm font-semibold capitalize">
+    <slot></slot>
+  </button>
+</template>
---
+++ feedback-form/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig, searchForWorkspaceRoot } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  server: {
+    fs: {
+      allow: [searchForWorkspaceRoot(process.cwd())]
+    }
+  }
+})
